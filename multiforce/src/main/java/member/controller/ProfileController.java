8c5f910f3247ab57b98453e073dd5aa745b6e64a
내거얌 <ababolove@gmail.com>
2024-01-12 17:03:40 +0900

    240112_soyoung

--- multiforce/src/main/java/member/controller/ProfileController.java
+++ multiforce/src/main/java/member/controller/ProfileController.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -62,23 +63,23 @@ public class ProfileController {
 	
 	
 	//마이 프로필 - 로그인 회원 정보
-	@RequestMapping(value = {"/myprofile"}, method = RequestMethod.GET)
+	@GetMapping("/myprofile")
 	public ModelAndView memberInfo (HttpSession session){		
 		//실제 로그인 시 수정할 부분
 		session.setAttribute("email", "test@test.com");	  
 	    
 		String email = (String) session.getAttribute("email");	
-		MemberDTO loginMember = memberservice.loginMember(email);		
-		ModelAndView mv = new ModelAndView();		
-		mv.addObject("loginMember",loginMember);
+		MemberDTO loginMember = memberservice.loginMember(email);	
 		
+		ModelAndView mv = new ModelAndView();		
+		mv.addObject("loginMember",loginMember);		
 		mv.setViewName("member/myprofile");
 		return mv;		
 	}
 		
 	
 	//마이 프로필 - 올린 프로젝트
-    @PostMapping("/getMyproject")
+    @GetMapping("/getMyproject")
     @ResponseBody
     List<ProjectDTO> getMyproject(HttpSession session) { 
 		//실제 로그인 시 수정할 부분
@@ -96,7 +97,7 @@ public class ProfileController {
     
     
     //마이 프로필 - 후원한 프로젝트
-    @PostMapping("/getFunded")
+    @GetMapping("/getFunded")
     @ResponseBody
     List<ProjectDTO> getFunded(HttpSession session) { 
 		//실제 로그인 시 수정할 부분
@@ -120,7 +121,7 @@ public class ProfileController {
 
     
     //마이 프로필 - 팔로워
-    @PostMapping("/getFollower")
+    @GetMapping("/getFollower")
     @ResponseBody
     List<MemberDTO> getFollower(HttpSession session) { 
 		//실제 로그인 시 수정할 부분
@@ -144,7 +145,7 @@ public class ProfileController {
     
     
     //마이 프로필 - 팔로잉
-    @PostMapping("/getFollowing")
+    @GetMapping("/getFollowing")
     @ResponseBody
     List<MemberDTO> getFollowing(HttpSession session) { 
 		//실제 로그인 시 수정할 부분
@@ -167,56 +168,47 @@ public class ProfileController {
  
 	
     
-    //후원한 프로젝트 페이지
-	@RequestMapping("/funded")
-	ModelAndView fundedProject (HttpSession session) {
-		//실제 로그인 시 수정할 부분
-		session.setAttribute("member_seq", 6);										
+    
+    //후원한 프로젝트용
+    private List<ProjectDTO> getFundedProjects(HttpSession session, List<FundingDTO> fundedList) { 
+        int memberSeq = (int) session.getAttribute("member_seq");
 
-		int memberSeq = (int)session.getAttribute("member_seq");		
+        List<Integer> projectSeqList = new ArrayList<>();
+        for (FundingDTO funded : fundedList) {
+            projectSeqList.add(funded.getProject_seq());
+        }
+        return projectservice.ongoingProject(projectSeqList);
+    }
+        
+    
+    // 후원한 프로젝트 페이지
+    @GetMapping("/funded")
+    ModelAndView fundedProject(HttpSession session) {
+        // 실제 로그인 시 수정할 부분
+        session.setAttribute("member_seq", 6);
 
-		List<FundingDTO> ongoingFunded = fundingservice.ongoingFunded(memberSeq); //후원 진행중
-		List<FundingDTO> successFunded = fundingservice.successFunded(memberSeq); //후원 성공
-		List<FundingDTO> cancelFunded = fundingservice.cancelFunded(memberSeq); //후원 취소
+        int memberSeq = (int) session.getAttribute("member_seq");
 
+        List<FundingDTO> ongoingFunded = fundingservice.ongoingFunded(memberSeq); // 후원 진행중
+        List<FundingDTO> successFunded = fundingservice.successFunded(memberSeq); // 후원 성공
+        List<FundingDTO> cancelFunded = fundingservice.cancelFunded(memberSeq); // 후원 취소
 
-		List<Integer> ongoingProjectSeq = new ArrayList<>();
-		for (FundingDTO projectSeq : ongoingFunded) {
-			ongoingProjectSeq.add(projectSeq.getProject_seq());
-		}
-		List<ProjectDTO> ongoingProject = projectservice.ongoingProject(ongoingProjectSeq);
-		//후원 진행중 ProjectDTO		
-		
-		
-		List<Integer> successProjectSeq = new ArrayList<>();
-		for (FundingDTO projectSeq : successFunded) {
-			successProjectSeq.add(projectSeq.getProject_seq());
-		}
-		List<ProjectDTO> successProject = projectservice.successProject(successProjectSeq);
-		// 후원 진행중 ProjectDTO		
-		
-		
-		List<Integer> cancelProjectSeq = new ArrayList<>();
-		for (FundingDTO projectSeq : cancelFunded) {
-			cancelProjectSeq.add(projectSeq.getProject_seq());
-		}
-		List<ProjectDTO> cancelProject = projectservice.cancelProject(cancelProjectSeq);
-		//후원 취소 ProjectDTO
+        List<ProjectDTO> ongoingProject = getFundedProjects(session, ongoingFunded);
+        List<ProjectDTO> successProject = getFundedProjects(session, successFunded);
+        List<ProjectDTO> cancelProject = getFundedProjects(session, cancelFunded);
 
-		
-		ModelAndView mv = new ModelAndView();
-		mv.addObject("ongoingFunded", ongoingFunded);		
-		mv.addObject("successFunded", successFunded);
-		mv.addObject("cancelFunded", cancelFunded);
-		mv.addObject("ongoingProject", ongoingProject);
-		mv.addObject("successProject", successProject);
-		mv.addObject("cancelProject", cancelProject);
-		
-		mv.setViewName("member/funded");
-		
-		//쿼리 조회문에 정렬 넣어야한다!!
-		return mv;
-	}
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("ongoingFunded", ongoingFunded);
+        mv.addObject("successFunded", successFunded);
+        mv.addObject("cancelFunded", cancelFunded);
+        mv.addObject("ongoingProject", ongoingProject);
+        mv.addObject("successProject", successProject);
+        mv.addObject("cancelProject", cancelProject);
+
+        mv.setViewName("member/funded");
+
+        return mv;
+    }
 
  
 
@@ -241,54 +233,95 @@ public class ProfileController {
 
 	
 	
+    //후원한 프로젝트 - 상세페이지
+    @GetMapping("/funded_detail/{fund_seq}")
+    ModelAndView fundedDetail(@PathVariable int fund_seq) {
+        int fundseq = fund_seq;  //후원번호
+        FundingDTO getFundedDetail = fundingservice.getFundedDetail(fundseq);	 		//후원정보
 
-	//후원 프로젝트 상세
-	@GetMapping("/funded_detail/{fund_seq}")			
-	ModelAndView fundedDetail (@PathVariable int fund_seq) {
-		int fundseq = fund_seq;//후원번호
-		FundingDTO getFundedDetail = fundingservice.getFundedDetail(fundseq); //후원정보		
-		int projectSeq = getFundedDetail.getProject_seq();//프로젝트번호
-		
-		ProjectDTO getProjectDetail = projectservice.getProjectDetail(projectSeq); //프로젝트정보
-		
-		LocalDateTime dueDate = getProjectDetail.getDue_date();
-		LocalDateTime currentTime = LocalDateTime.now();
-		int dDay = (int) ChronoUnit.DAYS.between(currentTime, dueDate); // 남은기한
+        int projectSeq = getFundedDetail.getProject_seq();								//프로젝트번호
+        ProjectDTO getProjectDetail = projectservice.getProjectDetail(projectSeq); 		//프로젝트정보
 
-				
-		//꾸러미가 여러개일 수 있다.
-		List<FundingBundleCountDTO> getCount = countservice.getCount(fundseq); //후원번호로 꾸러미 개수 찾기 -> 꾸러미 2000, 2001 2개.
-		List<Integer> bundleList = new ArrayList<>();
-		for (FundingBundleCountDTO seq : getCount) {
-			bundleList.add(seq.getBundle_seq());
-		}		
-				
-		List<BundleDTO> getBundle = bundleservice.getBundle(bundleList); //꾸러미 찾기		
-		List<ItemDTO> getItem = itemservice.getItem(bundleList);	//아이템 찾기
-		List<Integer> itemList = new ArrayList<>();
-		for (ItemDTO seq : getItem) {
-			itemList.add(seq.getItem_seq());
-		}
+        LocalDateTime dueDate = getProjectDetail.getDue_date();							// 남은기한
+        LocalDateTime currentTime = LocalDateTime.now();
+        int dDay = (int) ChronoUnit.DAYS.between(currentTime, dueDate);
 
-		List<ItemOptionDTO> getItemOption = itemoptionservice.getItemOption(itemList); //옵션찾기		
-		
-		
-		
-		
-		
-		
+        List<FundingBundleCountDTO> getCount = countservice.getCount(fundseq); 			//후원번호로 꾸러미개수 찾기
+        List<Integer> bundleList = new ArrayList<>(); 									//꾸러미 번호만 담긴 리스트
+        for (FundingBundleCountDTO seq : getCount) {
+            bundleList.add(seq.getBundle_seq());
+        }
 
-		ModelAndView mv = new ModelAndView();		
-		mv.addObject("fundedDetail", getFundedDetail); //후원정보
-		mv.addObject("projectDetail", getProjectDetail); //프로젝정보
-		mv.addObject("dDay", dDay); //남은기한
-		mv.addObject("getCount", getCount); //꾸러미개수
-		mv.addObject("getBundle", getBundle); //꾸러미정보
-		mv.addObject("getItem", getItem); //아이템정보
-		mv.addObject("getItemOption", getItemOption); //아이템의옵션		
-		mv.setViewName("member/funded_detail");		
-		return mv;
-	}	
+        List<BundleDTO> getBundle = bundleservice.getBundle(bundleList);				//꾸러미 찾기
+
+        // getBundle 리스트를 Map으로 변환
+        Map<Integer, String> bundleMap = getBundle.stream()
+                .collect(Collectors.toMap(BundleDTO::getBundle_seq, BundleDTO::getBundle_name));
+
+        List<ItemDTO> getItem = itemservice.getItem(bundleList);    					//아이템 찾기
+        List<Integer> itemList = new ArrayList<>();    									//아이템 번호만 담긴 리스트
+        for (ItemDTO seq : getItem) {
+            itemList.add(seq.getItem_seq());
+        }
+
+        Map<Integer, List<String>> bundleAndItem = new HashMap<>();						// 꾸러미 고유번호랑 아이템 이름 맵 생성
+        for (ItemDTO item : getItem) {
+            int bundleSeq = item.getBundle_seq();
+            String itemName = item.getItem_name();
+            bundleAndItem.computeIfAbsent(bundleSeq, k -> new ArrayList<>()).add(itemName);
+        }
+
+        Map<Integer, String> bundleNameMap = getBundle.stream()
+                .collect(Collectors.toMap(BundleDTO::getBundle_seq, BundleDTO::getBundle_name));
+      
+
+        Map<String, List<String>> bundleItem = bundleAndItem.entrySet().stream()
+                .collect(Collectors.toMap(entry -> bundleNameMap.get(entry.getKey()), entry -> entry.getValue()));       
+        
+        Map<String, Integer> bundleCount = getCount.stream()
+                .collect(Collectors.toMap(count -> bundleMap.get(count.getBundle_seq()), FundingBundleCountDTO::getPerchase_count));
+        
+
+        List<ItemOptionDTO> getItemOption = itemoptionservice.getItemOption(itemList);	//옵션찾기
+        Map<Integer, List<String>> itemAndOption = new HashMap<>(); 					// 아이템 고유번호랑 옵션 맵 생성
+        for (ItemOptionDTO option : getItemOption) {
+            int itemSeq = option.getItem_seq();
+            String optionName = option.getItem_option_name();
+            itemAndOption.computeIfAbsent(itemSeq, k -> new ArrayList<>()).add(optionName);
+        }
+
+        Map<Integer, String> itemNameMap = getItem.stream()
+                .collect(Collectors.toMap(ItemDTO::getItem_seq, ItemDTO::getItem_name));
+
+        Map<String, List<String>> itemOption = itemAndOption.entrySet().stream()
+                .collect(Collectors.toMap(entry -> itemNameMap.get(entry.getKey()), entry -> entry.getValue()));
+
+        String trackNum = (getFundedDetail.getTrack_num());   							//운송장번호.
+        if (getFundedDetail.getTrack_num() == null) {
+            trackNum = "0";
+        }
+
+        ModelAndView mv = new ModelAndView();
+
+        mv.addObject("trackNum", trackNum); // 운송장번호
+        mv.addObject("fundedDetail", getFundedDetail); //후원정보
+        mv.addObject("projectDetail", getProjectDetail); //프로젝정보
+        mv.addObject("dDay", dDay); //남은기한
+        mv.addObject("bundleCount", bundleCount); //꾸러미개수
+        mv.addObject("bundleItem", bundleItem); //번들-아이템
+        mv.addObject("itemOption", itemOption); //아이템-옵션
+
+        mv.setViewName("member/funded_detail");
+        return mv;
+    }
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	//후원 취소
 	@GetMapping("/funded_cancel")
--- multiforce/src/main/webapp/WEB-INF/views/member/funded_detail.jsp
+++ multiforce/src/main/webapp/WEB-INF/views/member/funded_detail.jsp
@@ -6,20 +6,27 @@
 <html>
 <head>
 <meta charset="UTF-8">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 <title>Insert title here</title>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+
+<style>
+
+
+</style>
+
 </head>
 
 <script>
 $(document).ready(function() {	
 
-	${fundedDetail.track_num}
 	if (${dDay} < 0) {
 		$("#pro_status").html("펀딩 종료")
 		$("#d_day").html("펀딩 종료")
 	}
 	
-	if (${fundedDetail.track_num }.equals() == null) {
-		#("track_num").html("등록된 운송장이 없습니다.")
+	if (${trackNum } == 0) {
+		$("#track_num").html("등록된 운송장이 없습니다.")
 	}
 	
 })
@@ -30,70 +37,107 @@ $(document).ready(function() {
 
 
 <body>
-
 <div>
-	<div style="border: 2px solid; ">
-		<div ><a href="${projectDetail.url }"><img alt="프로젝트 이미지" src="${projectDetail.main_images_url }"></a></div>
-		<div>
-			<div>${projectDetail.category}</div>
-			<div><a href="${projectDetail.url }">${projectDetail.long_title }</a></div>
-			<div>${projectDetail.collection_amount }원</div>
-			<div id="d_day">${dDay }일 남음</div>
-			<!-- 창작자 문의 버튼 만들어아햠 -->
+	<div class="content">
+	
+		<div style="border: 2px solid; ">
+			<div ><a href="${projectDetail.url }"><img alt="프로젝트 이미지" src="${projectDetail.main_images_url }"></a></div>
+			<div>
+				<div>${projectDetail.category}</div>
+				<div><a href="${projectDetail.url }">${projectDetail.long_title }</a></div>
+				<div>${projectDetail.collection_amount }원</div>
+				<div id="d_day">${dDay }일 남음</div>
+				<!-- 창작자 문의 버튼 만들어아햠 -->
+			</div>
+		</div>
+	
+		<div style="border: 2px solid; ">
+			<div>후원 정보</div>
+			<div>펀딩 상태 </div> <div id="pro_status">펀딩 진행중</div>
+			<div>후원 번호 </div> <div>${fundedDetail.fund_seq }</div>
+			<div>후원 날짜 </div> <div>${fundedDetail.fund_date.toLocalDate() }</div>
+			<div>펀딩 마감일 </div> <div>${projectDetail.due_date.toLocalDate()}</div>
+		</div>
+	
+		<div style="border: 2px solid; ">
+			<div>
+				<div>선물 정보</div>
+			</div>
+			 
+			<div>선물 구성 </div>
+			<div>
+			    <c:forEach var="bundleEntry" items="${bundleItem}">
+			        <div>꾸러미 이름: <strong>${bundleEntry.key}</strong> <span>${bundleCount[bundleEntry.key]}개</span> </div>
+			        <c:forEach var="itemEntry" items="${bundleEntry.value}">
+			            <div style="color: olive;">아이템 이름 : ${itemEntry}</div>
+			            <c:forEach var="optionEntry" items="${itemOption[itemEntry]}">
+			                <div style="color:navy; ">옵션 : ${optionEntry}</div>
+			            </c:forEach>            
+			        </c:forEach>
+			        <hr>
+			    </c:forEach>
+			</div>
+				
+			<div>배송 예정일 </div> <div>${projectDetail.delivery_date.toLocalDate()}</div>
+			<div>후원 금액 </div> <div>${fundedDetail.price }</div>
+		</div>
+	
+	
+		<div style="border: 2px solid; ">
+			<div>결제 정보</div> 
+			<div>결제 수단 </div> <div> ${fundedDetail.pay_option }</div>
+			<div>결제 금액 </div> <div> ${fundedDetail.price }</div>
+			<div>결제 상태 </div> <div> ${fundedDetail.fund_duedate.toLocalDate().plusDays(1)} 결제 예정</div>
+		</div>
+	
+	
+		<div style="border: 2px solid; ">
+			<div>배송 정보</div>
+			<div>받는 사람 </div> <div> ${fundedDetail.name}</div>
+			<div>연락처 </div> <div> ${fundedDetail.phone }</div><br>
+			<div>주소 </div> <div>(${fundedDetail.postalcode })</div> <div> ${fundedDetail.address }</div>
+			<div> ${fundedDetail.address_detail }</div>
+			<div>운송장 번호 </div> <div id="track_num"> ${trackNum }</div>
+			<p>
 		</div>
 	</div>
-<p>
-
-	<div style="border: 2px solid; ">
-		<div>후원 정보</div><br>
-		<div>펀딩 상태 </div> <div id="pro_status">펀딩 진행중</div><br>
-		<div>후원 번호 </div> <div >${fundedDetail.fund_seq }</div><br>
-		<div>후원 날짜 </div> <div >${fundedDetail.fund_date.toLocalDate() }</div><br>
-		<div>펀딩 마감일 </div> <div >${projectDetail.due_date.toLocalDate()}</div><br>
-	</div>
-<p>
-
-
-	<div style="border: 2px solid; ">
+			
 		<div>
-			<div>선물 정보</div>
-		</div>
-		 
-		<div>선물 구성 </div>
-		<c:forEach var="bundle" items="${getBundle }" varStatus="current">
-		</c:forEach>
-		<div >꾸러미 이름</div><br>
-			<ul>
-			<li> 아이템 이름 </li>
-				<div> 아이템</div>
-			</ul>
-		<div>배송 예정일 </div> <div>${projectDetail.delivery_date.toLocalDate()}</div>
-		<div>후원 금액 </div> <div >${fundedDetail.price }</div>
-	</div>
-<p>
-
-	<div style="border: 2px solid; ">
-		<div>결제 정보</div> 
-		<div>결제 수단 </div> <div> ${fundedDetail.pay_option }</div>
-		<div>결제 금액 </div> <div> ${fundedDetail.price }</div>
-		<div>결제 상태 </div> <div> ${fundedDetail.fund_duedate.toLocalDate().plusDays(1)} 결제 예정</div>
-	</div>
-<p>
-
-	<div style="border: 2px solid; ">
-		<div>배송 정보</div>
-		<div>받는 사람 </div> <div> ${fundedDetail.name}</div>
-		<div>연락처 </div> <div> ${fundedDetail.phone }</div><br>
-		<div>주소 </div> <div>(${fundedDetail.postalcode })</div> <div> ${fundedDetail.address }</div>
-		<div> ${fundedDetail.address_detail }</div><br>
-		<div>운송장 번호 </div> <div id="track_num" >${fundedDetail.track_num }</div>
-		<p>
+			<input type="button" onclick="location='../funded'" value="후원 목록 보기">			
+		</div>	
+	
+			<!-- Button trigger modal -->
+		<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+		  후원을 취소하시겠어요?
+		</button>
 		
-		<input type="button" value="후원 목록 보기">
-		<input type="button" value="후원을 취소하시겠어요?">
-		<!-- 후원 취소 다시묻는 모달창 -->
-	</div>
+		<!-- Modal -->
+		<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		  <div class="modal-dialog">
+		    <div class="modal-content">
+		      <div class="modal-header">
+		        <h1 class="modal-title fs-5" id="exampleModalLabel">이 프로젝트에는 후원자님이 필요해요</h1>
+		        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+			      </div>
+					<div class="modal-body">
+						<h2>후원 취소를 원하시나요?</h2>
+						<h5>혹시 선물 변경을 원하시나요?</h5>
+						후원을 취소하지 않고 원하는 선물로 변경할 수 있어요.<br>
+					  <p><a href="#" data-bs-toggle="tooltip" title="Tooltip">여기</a> 를 눌러 선물을 변경해보세요!</p>
+					</div>
+			      <div class="modal-footer">
+		        <button type="button" class="btn btn-primary" data-bs-dismiss="modal">후원 유지</button>
+		        <button type="button" class="btn btn-secondary">그래도 취소</button>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+	
+	
+
 </div>
 
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
 </html>
