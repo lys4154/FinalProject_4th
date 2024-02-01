14a8e21eae192f789e78494c2871cd80cd8629fe
lys4154 <lys4154@gmail.com>
2024-02-01 17:11:08 +0900

    240201Commit

--- multiforce/src/main/java/project/dao/FundingBundleCountDAO.java
+++ multiforce/src/main/java/project/dao/FundingBundleCountDAO.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Repository;
 import funding.dto.FundingDTO;
 import project.dto.BundleDTO;
 import project.dto.FundingBundleCountDTO;
+import project.dto.ProjectDTO;
 
 @Repository
 @Mapper
@@ -17,7 +18,12 @@ public interface FundingBundleCountDAO {
 	//후원 상세 - 꾸러미 개수 
 	List<FundingBundleCountDTO> getCount(int fundseq);
 
-
+	// 번들 작성
+	void insertBundle(BundleDTO bundle);
+		
+	//member_seq불러오기
+	int getProject_seq(String long_title);
+	
 
 
 }
